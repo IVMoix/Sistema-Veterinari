e7ec15e0657975254b20f3bb086df978615bf8f8
Iñaki Vladimir Moix <iniakivmoix@gmail.com>
2023-11-16 13:32:20 -0300

    Crear paciente component

--- src/app/paginas/crear-paciente/crear-paciente.component.ts
+++ src/app/paginas/crear-paciente/crear-paciente.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PacientesService } from 'src/app/servicios/pacientes.service';
 
 @Component({
   selector: 'app-crear-paciente',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class CrearPacienteComponent {
 
+  pacientes: any = {};
+
+  constructor (public pacientesService: PacientesService) {}
+
+  
+  altaPaciente(){
+
+    let formData = new FormData();
+    formData.append('nompaciente',this.pacientes.nompaciente);
+    formData.append('edadpaciente',this.pacientes.edadpaciente);
+    formData.append('telpaciente',this.pacientes.telpaciente);
+    formData.append('dirpaciente',this.pacientes.dirpaciente);
+
+    this.pacientesService.metodoPost('AltaPaciente.php', formData).subscribe((event: any) =>{
+      console.log(event);
+      if(event.status == 'success') {
+        
+      }
+    } 
+    )
+  }
 }
