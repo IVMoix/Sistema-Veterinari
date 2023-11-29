d5ad38dbeca18990bfc4ac0a7dbb6a6604877bf4
Iñaki Vladimir Moix <iniakivmoix@gmail.com>
2023-11-29 15:55:55 -0300

    Firebase implementation

--- src/app/apiServer.ts
+++ src/app/apiServer.ts
@@ -1,4 +1,4 @@
 export const apiServer = {
-    URL: 'http://localhost/servidor_medico/'
-    // URL: 'https://sistemadeventas.com.ar/moix/'
+    //URL: 'http://localhost/servidor_medico/'
+    URL: 'https://sistemadeventas.com.ar/moix/servidor_medico/'
 }
